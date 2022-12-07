cd7ec4bbe591c31213c395a207e4b3d9627e19c7
Josset <josset.garcia@ip-192-168-0-5.ap-southeast-1.compute.internal>
2022-12-06 22:11:13 -0300

    [WIP] add /user endpoint to insert new users

--- server/server.go
+++ server/server.go
@@ -1,9 +1,27 @@
 package server
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+}
+
+func InsertUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	b, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if !json.Valid(b) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
--- server/server_test.go
+++ server/server_test.go
@@ -3,6 +3,7 @@ package server_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 
 	"cl.isset.userfy/server"
 	. "github.com/onsi/ginkgo/v2"
@@ -24,4 +25,46 @@ var _ = Describe("Server", func() {
 		})
 	})
 
+	Describe("/user endpoint", func() {
+		Context("When inserting a new user", func() {
+			var validUserJson string
+			var userReader *strings.Reader
+			var request *http.Request
+			var recorder *httptest.ResponseRecorder
+
+			BeforeEach(func() {
+				validUserJson = `{"ID": 12345, "Name": "Josset", "Email": "isset.josset@gmail.com", "Age": 26}`
+				userReader = strings.NewReader(validUserJson)
+				request = httptest.NewRequest(http.MethodPost, "/user", userReader)
+				recorder = httptest.NewRecorder()
+			})
+
+			It("Should return a 201 status code if payload is valid", func() {
+				server.InsertUserHandler(recorder, request)
+
+				result := recorder.Result()
+				defer result.Body.Close()
+				Expect(result.StatusCode).To(Equal(201))
+			})
+
+			It("Should return a 405 status code if method is not allowed", func() {
+				request := httptest.NewRequest(http.MethodGet, "/user", userReader)
+				server.InsertUserHandler(recorder, request)
+
+				result := recorder.Result()
+				defer result.Body.Close()
+				Expect(result.StatusCode).To(Equal(405))
+			})
+
+			It("Should return a 400 status code when payload is not valid", func() {
+				userReader := strings.NewReader(`"{ID": 1234, {}`)
+				request := httptest.NewRequest(http.MethodPost, "/user", userReader)
+				server.InsertUserHandler(recorder, request)
+
+				result := recorder.Result()
+				defer result.Body.Close()
+				Expect(result.StatusCode).To(Equal(400))
+			})
+		})
+	})
 })
