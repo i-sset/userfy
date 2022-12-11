539e0c92aa55d01b0da7cb0c49e6c0664279a5fb
Josset <josset.garcia@ip-192-168-0-5.ap-southeast-1.compute.internal>
2022-12-11 15:41:09 -0300

    attach function handler to /users endpoint

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", server.RootHandler)
 	http.HandleFunc("/user", server.InsertUserHandler)
+	http.HandleFunc("/users", server.GetUsersHandler)
 	fmt.Println("Listening at port 8080")
 	http.ListenAndServe(":8080", nil)
 }
