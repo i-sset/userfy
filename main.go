b13bb1c28771e2007bb3d70e9e3ff748b059bde1
Josset <josset.garcia@ip-192-168-0-5.ap-southeast-1.compute.internal>
2022-12-12 00:00:15 -0300

    add loadDatabae function to insert example data

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"cl.isset.userfy/model"
+	"cl.isset.userfy/repository"
+
 	"cl.isset.userfy/server"
 )
 
@@ -13,6 +16,18 @@ func main() {
 	http.HandleFunc("/user/update", server.UpdateUserHandler)
 	http.HandleFunc("/users", server.GetUsersHandler)
 
+	loadDatabase()
 	fmt.Println("Listening at port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func loadDatabase() {
+	userRepository := repository.UserRepository{}
+
+	userRepository.InsertUser(model.User{Name: "Josset Garcia", Email: "isset.joset@gmail.com", Age: 26})
+	userRepository.InsertUser(model.User{Name: "Silvana Ferreiro", Email: "silvanaf@thoughtworks.com", Age: 42})
+	userRepository.InsertUser(model.User{Name: "Nicolas Bedregal", Email: "nicobe@gmail.com", Age: 32})
+	userRepository.InsertUser(model.User{Name: "Javiera Lasus", Email: "javivu@thoughtworks.com", Age: 27})
+	userRepository.InsertUser(model.User{Name: "Adriana Ortega", Email: "adriortega@gmail.com", Age: 30})
+
 }
